e33f16d4c8f54a331fc677011ef9f5ddf1272e05
Neekrhael <neekrhael@gmail.com>
2014-10-05 17:44:42 -0500

    10/05/14 Switching to Web-Based Implementation

---
+++ wof.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <!-- Essential Entries -->
+        <meta charset="UTF-8">
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/jquery-ui.min.js"></script> 
+
+        <!-- JS Entries -->
+
+        <!-- CS Entries -->
+
+    </head>
+
+    <body>
+
+        <?PHP
+            require './gui/main_window.php';    //Primary GUI Require
+        ?>
+
+    </body>
+</html>
