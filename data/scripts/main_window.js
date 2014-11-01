f8319a76b811f43bff4c4df9994d40bb65b78ea6
Neekrhael <neekrhael@gmail.com>
2014-11-01 17:48:17 -0500

    11/1/14 pt. 2

---
+++ data/scripts/main_window.js
@@ -0,0 +1,18 @@
+$(function() {
+
+    $("#topCharacterPan").click(function() {
+        alert("Top Character Pan Tile")
+    });
+
+    $("#bottomCharacterPan").click(function() {
+        alert("Bottom Character Pan Tile")
+    });
+
+    $("#topAbilityPan").click(function() {
+        alert("Top Ability Pan Tile")
+    });
+
+    $("#bottomAbilityPan").click(function() {
+        alert("Bottom Ability Pan Tile")
+    });
+});
